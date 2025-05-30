4b2e2bd63c6f98cbe30da19f73a5620cb7e097b1
Marouane Bachar <marstrone@gmail.com>
2025-05-30 23:48:32 +0200

    add new user to the database  /add

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const app = express();
 const port =5000;
 require("dotenv").config();
-
+app.use(express.json());
 mongoose.connect(process.env.mongdb);
 
 const userSchema = new mongoose.Schema(
@@ -26,6 +26,17 @@ app.get("/",(req,res)=>{
         res.json(data)
     })
 })
+
+app.post("/add", async (req, res) => {
+    const newUser = new userModel({
+        name: req.body.name,
+        age: req.body.age,
+        email: req.body.email
+    });
+
+    await newUser.save();
+    res.json({ message: "User added successfully", user: newUser });
+});
 
 user1.save();
 app.listen(port,()=>{
